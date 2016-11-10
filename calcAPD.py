114e9617e00bcb5bfd6707823c67456dbdba04c9
Alexandra Diana Badiceanu <alexbadiceanu22@gmail.com>
2016-11-10 11:47:38 -0500

    Update calcAPD.py

--- calcAPD.py
+++ calcAPD.py
@@ -1,49 +1,58 @@
 #!/usr/bin/env python
-
+#Script used on waveform data to determine the duration of 
+#action potentials. APDs are computed as the time point of 
+#maximal upstroke velocity (dV/dt)max minus the time point of 
+#x% repolarization of the membrane potential (APDx).  
 import numpy as np
 
+#function used to compute APD
+#takes in a file containing the waveform and
+#the percent of repolarization desired
 def calculate_apd (data_file, perc):
 
     data = np.loadtxt(data_file)            # format: %f %f \n (time voltage)
-    tm = data[:,0]                          # time
-    vm = data[:,1]                          # transmembrane potential
+    time = data[:,0]                          # time
+    Vm = data[:,1]                          # transmembrane potential
 
     ### find the activation time
     idmax = 0
     dvmax = 0.0
-    dt = tm[1] - tm[0]
-    size = len(vm)-1
+    dt = time[1] - time[0]
+    size = len(Vm)-1
 
     for i in xrange(size):
-        dv = vm[i+1] - vm[i]
+        dv = Vm[i+1] - Vm[i]
         if (dv > dvmax):
             dvmax = dv/dt
             idmax = i
 
-    act_time = tm[idmax]
+    act_time = time[idmax]
 
-    amp = np.max(vm) - np.min(vm)           # find the AP amplitude
+    amp = np.max(Vm) - np.min(Vm)           # find the AP amplitude
 
     #### find the repolarization time
-    vchk = amp*(1.0-perc) + np.min(vm)
+    vchk = amp*(1.0-perc) + np.min(Vm)
     stop = False
     for i in xrange(size):
-        dv = vm[i+1] - vm[i]
-        if ((dv<0.) and (vm[i] < vchk) and (not stop)):
-            rep_time = tm[i]
+        dv = Vm[i+1] - Vm[i]
+        if ((dv<0.) and (Vm[i] < vchk) and (not stop)):
+            rep_time = time[i]
             stop = True
 
-    apd = np.abs(rep_time - act_time)       # calculate APD
+    APD = np.abs(rep_time - act_time)       # calculate APD
 
     print 'APD %d   ' % (perc*100),
-    print '%08.4f ms' % apd
-    return apd
+    print '%08.4f ms' % APD
+    return APD
 
 if __name__ == "__main__":
 
     import doctest
     doctest.testmod()
 
+    
+#For most experiments we require APDs for multiple % repolarization
+#Therefore, the script shows APDs from 50 to 95%
 calculate_apd ("test.dat", 0.95)
 calculate_apd ("test.dat", 0.9)
 calculate_apd ("test.dat", 0.85)
